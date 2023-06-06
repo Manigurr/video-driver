yaap/kernel_oneplus_sm8650-modules

video: driver: Add new hfi buffer flags
New hfi buffer flags to send memory region information to firmware.

Change-Id: I3a320622d0ed01a5e583dbeceb2648a57f618a66
Signed-off-by: Chinmay Sawarkar <quic_chinmays@quicinc.com>

driver/vidc/src/msm_vidc_driver.c
@@ -2524,6 +2524,7 @@ int msm_vidc_allocate_buffers(struct msm_vidc_inst *inst,
 		list_add_tail(&buf->list, &buffers->list);
 		buf->type = buf_type;
 		buf->index = idx;
+		buf->region = call_mem_op(core, buffer_region, inst, buf_type);
 	}
 	i_vpr_h(inst, "%s: allocated %d buffers for type %s\n",
 		__func__, num_buffers, buf_name(buf_type));
@@ -3295,6 +3296,7 @@ int msm_vidc_create_internal_buffer(struct msm_vidc_inst *inst,
 
 	buffer->dmabuf = mem->dmabuf;
 	buffer->device_addr = mem->device_addr;
+	buffer->region = mem->region;
 	i_vpr_h(inst, "%s: create: type: %8s, size: %9u, device_addr %#llx\n", __func__,
 		buf_name(buffer_type), buffers->size, buffer->device_addr);
 

Manigurr/video-driver

driver/vidc/src/msm_vidc_driver.c
@@ -2867,6 +2867,7 @@ int msm_vidc_allocate_buffers(struct msm_vidc_inst *inst,
 		list_add_tail(&buf->list, &buffers->list);
 		buf->type = buf_type;
 		buf->index = idx;
+		buf->region = call_mem_op(core, buffer_region, inst, buf_type);
 	}
 	i_vpr_h(inst, "%s: allocated %d buffers for type %s\n",
 		__func__, num_buffers, buf_name(buf_type));
@@ -3673,6 +3674,7 @@ int msm_vidc_create_internal_buffer(struct msm_vidc_inst *inst,
 
 	buffer->dmabuf = alloc->dmabuf;
 	buffer->device_addr = map->device_addr;
+	buffer->region = map->region;
 	i_vpr_h(inst, "%s: create: type: %8s, size: %9u, device_addr %#llx\n", __func__,
 		buf_name(buffer_type), buffers->size, buffer->device_addr);
 