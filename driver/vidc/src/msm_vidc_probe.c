1347f70e1c1aba349ceafb286faf92c4e9b5479c
Albin Robin <quic_albirobi@quicinc.com>
2024-12-18 11:03:45 -0500

    video-driver: Update driver compatible string for nordau
    Update compatible string checks to include nordau.
    Include resource ops in nordau init.
    
    Change-Id: If43e0ab2fc022bef611ca8fed0d5c69ed27abdd2
    Signed-off-by: Albin Robin <quic_albirobi@quicinc.com>

--- driver/vidc/src/msm_vidc_probe.c
+++ driver/vidc/src/msm_vidc_probe.c
@@ -44,6 +44,7 @@ static inline bool is_video_device(struct device *dev)
 		of_device_is_compatible(dev->of_node, "qcom,sm8550-vidc") ||
 		of_device_is_compatible(dev->of_node, "qcom,sm8550-vidc-v2") ||
 		of_device_is_compatible(dev->of_node, "qcom,sa8255-vidc") ||
+		of_device_is_compatible(dev->of_node, "qcom,sa8797-vidc") ||
 		of_device_is_compatible(dev->of_node, "qcom,sm8650-vidc"));
 }
 
@@ -122,6 +123,7 @@ static const struct of_device_id msm_vidc_dt_match[] = {
 	{.compatible = "qcom,sm8550-vidc-v2"},
 	{.compatible = "qcom,sm8650-vidc"},
 	{.compatible = "qcom,sa8255-vidc"},
+	{.compatible = "qcom,sa8797-vidc"},
 	{.compatible = "qcom,vidc,cb-ns-pxl"},
 	{.compatible = "qcom,vidc,cb-ns"},
 	{.compatible = "qcom,vidc,cb-sec-non-pxl"},
